b6a3ac83f2b045936a72c954ea2866b5ddfc063b
Haiqa Ashraf <haiqa@primary-portal.com>
2023-06-01 17:36:06 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        //just add this below and your all other tailwind colors willwork
+        ...colors,
+      },
+      listStyleType: {
+        none: "none",
+        disc: "disc",
+        decimal: "decimal",
+        square: "square",
+        roman: "upper-roman",
+      },
+    },
+  },
+  plugins: [],
+};
